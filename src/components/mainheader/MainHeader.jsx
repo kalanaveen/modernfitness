3bec594d4ebe917e8db3ac9dde18f815f45553bb
naveen kala <kalanaveen55@gmail.com>
2023-03-02 16:55:58 +0530

    main header complete

--- src/components/mainheader/MainHeader.jsx
+++ src/components/mainheader/MainHeader.jsx
@@ -6,6 +6,7 @@ const MainHeader = () => {
   return (
     <header className="main__header">
       <div className="container main__header-container">
+        {/* left header  */}
         <div className="main__header-left">
           <h4>#100DaysOfWorkout</h4>
           <h1>join The Modern Fitness World</h1>
@@ -13,10 +14,14 @@ const MainHeader = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi,
             provident impedit laboriosam nesciunt asperiores blanditiis?
           </p>
-          <Link to='/plans' className='btn lg'>Get Started</Link>
+          <Link to="/plans" className="btn lg">
+            Get Started
+          </Link>
         </div>
+
+        {/* right header  */}
         <div className="main__header-right">
-          <div className="amin__header-circle"></div>
+          <div className="main__header-circle"></div>
           <div className="main__header-image">
             <img src={Image} alt="header__image" />
           </div>
