cc1178b94554ccafb07ad3f69315aaee8d93e68e
naveen kala <kalanaveen55@gmail.com>
2023-03-09 19:50:45 +0530

    values component added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import MainHeader from '../../components/mainheader/MainHeader';
 import Programs from '../../components/programs/Programs';
+import Values from '../../components/values/Values';
 import './home.css';
 
 const Home = () => {
   return (
     <div>
       <MainHeader />
-      <Programs/>
+      <Programs />
+      <Values/>
     </div>
   )
 }
