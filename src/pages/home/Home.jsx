cfd5ace44ba91e82e10398473869c48fbaec6eb9
naveen kala <kalanaveen55@gmail.com>
2023-03-10 13:35:16 +0530

    testimonial added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import FAQs from '../../components/faqs/FAQs';
 import MainHeader from '../../components/mainheader/MainHeader';
 import Programs from '../../components/programs/Programs';
+import Testimonials from '../../components/testimonials/Testimonials';
 import Values from '../../components/values/Values';
 import './home.css';
 
@@ -10,7 +11,8 @@ const Home = () => {
       <MainHeader />
       <Programs />
       <Values />
-      <FAQs/>
+      <FAQs />
+      <Testimonials/>
     </div>
   )
 }
